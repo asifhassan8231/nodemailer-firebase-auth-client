c7c0dfe6a07c8f273e33a00ef7f96d4e3c54b01e
asifhassan8231 <asifhassan8231@gmail.com>
2021-11-22 15:23:51 +0600

    firebase config set

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCfeykeWhsKOtwaGFpzZOEpZwana2v4yBE",
+    authDomain: "nodemailer-firebase-auth.firebaseapp.com",
+    projectId: "nodemailer-firebase-auth",
+    storageBucket: "nodemailer-firebase-auth.appspot.com",
+    messagingSenderId: "444683980072",
+    appId: "1:444683980072:web:38b35531bd0d63b3d2d41d"
+};
+
+export default firebaseConfig;
